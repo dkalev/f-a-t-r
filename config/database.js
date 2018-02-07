adb284b1121f256d02d08582cb613b015d40d153
Damyan Kalev <dzkalev@gmail.com>
2018-02-07 22:33:15 +0800

    Updated with correct db uri

--- config/database.js
+++ config/database.js
@@ -1,3 +1,3 @@
 module.exports = {
-    remoteUrl : 'mongodb://fatr:asweng_8102@mongo.onmodulus.net:27017/uwO3mypu'
+    remoteUrl : 'mongodb://fatr:asweng_8102@ds129018.mlab.com:29018/heroku_ww8q054j'
 };
