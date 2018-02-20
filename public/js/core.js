66e26c6fa0d1960ea75d3e2791e8d9ed7169c5e1
Damyan Kalev <dzkalev@gmail.com>
2018-02-20 17:34:19 +0800

    Added templates

--- public/js/core.js
+++ public/js/core.js
@@ -1,8 +1,15 @@
-angular.module('scotchTodo', [
-  'usersController', 
-  'bookingsController',
-  'tutorialRoomsController',
-  'bookingService',
-  'tutorialRoomService',
-  'userService'
-]);
+var app = angular.module('mainApp', [
+  'ui.router'
+]).config(['$urlRouterProvider', '$stateProvider', function($urlRouterProvider,$stateProvider){
+  $urlRouterProvider.otherwise('/');
+
+  $stateProvider
+    .state('home', {
+      url: '/',
+      templateUrl: 'templates/home.html'
+    })
+    .state('bookTutorial', {
+      url: '/',
+      templateUrl: 'templates/bookTutorial.html'
+    });
+}]);
