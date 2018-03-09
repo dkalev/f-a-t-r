5b984427cf2295dec7975925c614fb01357e66a9
Damyan Kalev <dzkalev@gmail.com>
2018-03-09 12:48:12 +0800

    Added user authentication and sessions

--- app/models/user.js
+++ app/models/user.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
 var ObjectId = mongoose.Schema.Types.ObjectId;
 
 //todo: add validations: unique, required, regex
@@ -23,4 +24,41 @@ var UserSchema = new mongoose.Schema({
   bookings: [{ type : ObjectId }]
 }, {timestamps: true});
 
-module.exports = mongoose.model('User', UserSchema);
+//prehook to hash the password before inserting into db
+UserSchema.pre('save', function(next){
+  var user = this;
+  bcrypt.hash(user.password, 10, function (err, hash){
+    if (err) {
+      return next(err);
+    }
+    user.password = hash;
+    next();
+  })
+});
+
+//authenticate and login user
+UserSchema.statics.authenticate = function(email, password, callback) {
+  User.findOne({email: email})
+    .exec(function (err, user){
+      if(err){
+        return callback(err)
+      //if there is no such user throw an error
+      }else if(!user){
+        var err = new Error('Username or password incorrect');
+        err.status = 401;
+        return callback(err);
+      }
+      //otherwise compare input password and user password
+      bcrypt.compare(password, user.password, function(err, result){
+        if (result){
+          //return the user data if the passwords match
+          return callback(null, user);
+        }else{
+          return callback();
+        }
+      })
+    });
+}
+
+var User = mongoose.model('User', UserSchema);
+module.exports = User;
--- app/routes/userRoutes.js
+++ app/routes/userRoutes.js
@@ -9,17 +9,69 @@ module.exports = function (app) {
       res.json(users);
     });
   });
-  app.post('/api/user', function(req, res) {
-    User.create({
 
-        //TODO we need user_id and tutorialroom_id => query the db at some point
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password
-    }, function (err, todo) {
-        if (err)
-          res.send(err);
-    });
+
+  app.post('/api/user', function(req, res) {
+
+    //if a new registration
+    if (req.body.username &&
+      req.body.email &&
+      req.body.password) {
+        var userData = {
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password
+        }
+
+      User.create(userData , function (err, user) {
+          if (err){
+            res.send(err);
+          }else{
+            //create a session with the newly created user
+            req.session.userId = user._userId;
+            res.redirect('/profile');
+          }
+      });
+
+    //if an user is logging in
+    }else if (req.body.logemail && req.body.logpassword) {
+        User.authenticate(req.body.logemail, req.body.logpassword, function (error, user){
+          if (error || !user) {
+            next(new Error('Wrong email or password'));
+          }else{
+            req.session.userId = user._userId;
+            res.redirect('profile');
+          }
+        });
+      //otherwise some of the form fields must be missing
+      } else {
+        return next(new Error('Please fill all fields.'))
+      }
+  });
+
+  app.get('api/profile', function(req, res){
+    User.findById(req.session.userId)
+      .exec(function (err, user){
+        if(err){
+          next(err);
+        }else{
+          if (user != null) {
+            //show content available only to registered users
+            res.send('<h1>Name: </h1>' + user.username + '<h2>Mail: </h2>' + user.email + '<br><a type="button" href="/logout">Logout</a>')
+          }
+        }
+      })
+  });
+
+  //upon logout destroy user session
+  app.get('api/logout', function(req, res){
+    if(req.session){
+      req.session.destroy(function (err) {
+        if(err)
+          next(err);
+        res.redirect('/');
+      })
+    }
   });
 
   // delete an user
